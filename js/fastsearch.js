8b27979f6c123ef38742979d45caf6b32b334438
unknown <theexgenesis@gmail.com>
2020-03-18 00:51:54 +0000

    fast search

--- js/fastsearch.js
+++ js/fastsearch.js
@@ -130,12 +130,13 @@ function executeSearch(term) {
     searchitems = '';
   } else { // build our html 
     for (let item in results.slice(0,5)) { // only show first 5 results
-      if(results[item].date){ //wiki pages have no date
-          var date = results[item].date;
+      r = results[item].item
+      if(r.date){ //wiki pages have no date
+          var date = r.date;
       }else{
         var date = "";
       };
-      searchitems = searchitems + '<li><a href="' + results[item].url + '" tabindex="0">' + '<span class="title"><strong>' + results[item].title + '</strong></span><br /> ' + date + ' — <em>' + results[item].content.substring(0,90) + '</em></a></li>';
+      searchitems = searchitems + '<li><a href="' + r.url + '" tabindex="0">' + '<span class="title"><strong>' + r.title + '</strong></span><br /> ' + date + ' — <em>' + r.content.substring(0,90) + '</em></a></li>';
     }
     resultsAvailable = true;
   }
